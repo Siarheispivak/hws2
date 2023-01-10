37aea5036222fc8ce31b36b241ee4c0479567241
Siarheispivak <siarheispivak2@gmail.com>
2023-01-10 23:13:37 +0300

    HW7 is almost done,still need to fix classes

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,44 @@
-import {UserType} from '../HW8'
+// import {UserType} from '../HW8'
+
+import {UserType} from "../HW8";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
+            if (action.payload === 'up') {
+                const copyState = [...state]
+                const sortedArray = copyState.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    return 0;
+                })
+                return sortedArray
+            } else if (action.payload === 'down') {
+                const copyState = [...state]
+                const sortedArray = copyState.sort(function (a, b) {
+                    if (a.name < b.name) {
+                        return 1;
+                    }
+                    if (a.name > b.name) {
+                        return -1;
+                    }
+                    return 0;
+                })
+                return sortedArray
+            }
             return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            const olderThenEighteen = [...state].filter(el => el.age >= 18);
+            return olderThenEighteen // need to fix
         }
         default:
             return state
